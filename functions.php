eec148d28ecd58697f4bbec4f1935151f9bbb6cc
Bhavye Mathur <bhavyemathur@gmail.com>
2023-06-17 22:38:56 +0530

    Set-up block theme
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

--- functions.php
+++ functions.php
@@ -3,11 +3,9 @@
 add_theme_support("align-wide");
 add_theme_support( "menus" );
 add_theme_support( "custom-logo" );
-add_theme_support( "post-thumbnails" );
 
 remove_theme_support("core-block-patterns");
 
-add_theme_support( "editor-styles" );
 add_editor_style( "assets/css/main.css" );
 
 require_once get_theme_file_path( "inc/remove-admin-bar.php" );
@@ -16,7 +14,7 @@ require_once get_theme_file_path( "inc/load-styles.php" );
 require_once get_theme_file_path( "inc/load-scripts.php" );
 require_once get_theme_file_path( "inc/load-fonts.php" );
 
-require_once get_theme_file_path( "inc/register-iispmun_editions.php" );
+// require_once get_theme_file_path( "inc/register-iispmun_editions.php" );
 //require_once get_theme_file_path( "inc/register-iispmun_committees.php" );
 //require_once get_theme_file_path( "inc/register-iispmun_people.php" );
 //require_once get_theme_file_path( "inc/register-iispmun_socials.php" );
--- inc/register-block-patterns.php
+++ inc/register-block-patterns.php
@@ -2,69 +2,7 @@
 
 function iispmun_register_patterns()
 {
-    register_block_pattern(
-        "iispmun/mun-theme",
-        array(
-            "title" => __("MUN Theme", "iispmun"),
-            "content" => file_get_contents(get_theme_file_path("patterns/mun-theme.html")),
-        )
-    );
 
-    register_block_pattern(
-        "iispmun/primary-accented-heading",
-        array(
-            "title" => __("Primary Accented Heading", "iispmun"),
-            "content" => file_get_contents(get_theme_file_path("patterns/primary-accented-heading.html")),
-        )
-    );
-
-    register_block_pattern(
-        "iispmun/secondary-accented-heading",
-        array(
-            "title" => __("Secondary Accented Heading", "iispmun"),
-            "content" => file_get_contents(get_theme_file_path("patterns/secondary-accented-heading.html")),
-        )
-    );
-
-    register_block_pattern(
-        "iispmun/hover-card",
-        array(
-            "title" => __("Hover Card", "iispmun"),
-            "content" => file_get_contents(get_theme_file_path("patterns/hover-card.html")),
-        )
-    );
-
-    register_block_pattern(
-        "iispmun/hover-card-with-image",
-        array(
-            "title" => __("Hover Card with Image", "iispmun"),
-            "content" => file_get_contents(get_theme_file_path("patterns/hover-card-with-image.html")),
-        )
-    );
-
-    register_block_pattern(
-        "iispmun/committee-hover-card",
-        array(
-            "title" => __("Committee Hover Card", "iispmun"),
-            "content" => file_get_contents(get_theme_file_path("patterns/committee-hover-card.html")),
-        )
-    );
-
-    register_block_pattern(
-        "iispmun/mun-committees",
-        array(
-            "title" => __("IISP MUN Committees", "iispmun"),
-            "content" => file_get_contents(get_theme_file_path("patterns/mun-committees.html")),
-        )
-    );
-
-    register_block_pattern(
-        "iispmun/instagram-feed",
-        array(
-            "title" => __("IISP MUN Instagram Feed", "iispmun"),
-            "content" => file_get_contents(get_theme_file_path("patterns/instagram-feed.html")),
-        )
-    );
 }
 
 add_action("init", "iispmun_register_patterns");
