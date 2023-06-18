d70301e13e0698f61e4ae0dd625831826159482b
Bhavye Mathur <bhavyemathur@gmail.com>
2023-06-18 22:41:32 +0530

    Made IISP MUN Committees automatically load data from committees post type
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

--- blocks/iispmun-committees-slideshow/block.php
+++ blocks/iispmun-committees-slideshow/block.php
@@ -1,26 +1,34 @@
-<!--Adapted From https://www.codingnepalweb.com/draggable-image-slider-html-css-javascript/-->
-
 <section id="iispmun-committees-slideshow">
     <div class="iispmun-committees-slideshow-wrapper">
         <i id="left" class="glyphicon glyphicon-arrow-left expand-on-hover"></i>
         <div class="iispmun-committees-slideshow">
-            <?php for ($i = 0; $i < 7; $i++): ?>
+            <?php
+            $args = array(
+                "post_type"      => "iispmun_committees",
+                "posts_per_page" => -1,
+            );
+            
+            $loop = new WP_Query($args);
+
+            while ( $loop->have_posts() ):
+                $loop->the_post();
+                ?>
                 <div class="iispmun-committees-slideshow-card-container">
                     <div class="iispmun-committees-slideshow-card expand-on-hover">
-                        <img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2016/11/johnny-depp-as-jack-sparrow-in-pirates-of-the-caribbean.jpg">
+                        <img src="<?php the_post_thumbnail(); ?>">
                         <div class="iispmun-committees-slideshow-card-title">
-                            <p>UNHRC</p>
-                            <small>Intermediate</small>
+                            <p><?php the_field("acronym"); ?></p>
+                            <small><?php the_field("difficulty"); ?></small>
                         </div>
                     </div>
                     <div class="iispmun-committees-slideshow-subtitle">
                         <p>
                             <span>Topic</span>
-                            <span>Ethicality of the Death Penalty</span>
+                            <span><?php the_field("short_agenda"); ?></span>
                         </p>
                     </div>
                 </div>
-            <?php endfor; ?>
+            <?php endwhile; ?>
         </div>
         <i id="right" class="glyphicon glyphicon-arrow-right expand-on-hover"></i>
     </div>
