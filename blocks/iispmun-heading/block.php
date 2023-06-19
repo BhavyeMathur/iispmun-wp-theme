6620707724e47249c20c5576f0c06102d6610b91
Bhavye Mathur <bhavyemathur@gmail.com>
2023-06-19 17:16:24 +0530

    Fixed issue with IISP MUN heading block not colouring its text properly
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

--- blocks/iispmun-heading/block.php
+++ blocks/iispmun-heading/block.php
@@ -1,16 +1,8 @@
 <h1 style="font-weight: 900; margin: 40px 0;">
-    <span><?php block_field("text-1"); ?></span>
-    <span><?php block_field("text-2"); ?></span>
+    <span style="color: <?php block_field("color-1"); ?>"><?php block_field("text-1"); ?></span>
+    <span style="color: <?php block_field("color-2"); ?>"><?php block_field("text-2"); ?></span>
 </h1>
 
 <style>
     <?php include "style.css"; ?>
-
-    h1 span:first-child {
-        color: <?php block_field("color-1"); ?>;
-    }
-
-    h1 span:last-child {
-        color: <?php block_field("color-2"); ?>;
-    }
 </style>
