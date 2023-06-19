dcdd548b92669be91a94b7587ef0386a7fb39dc2
Bhavye Mathur <bhavyemathur@gmail.com>
2023-06-20 00:25:35 +0530

    Added a committees grid block
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

---
+++ blocks/iispmun-committees-grid/block.php
@@ -0,0 +1,38 @@
+<section id="iispmun-committees-slideshow">
+    <div class="iispmun-committees-cards-wrapper">
+        <div class="iispmun-committees-cards-container">
+            <?php
+            $args = array(
+                "post_type"      => "iispmun_committees",
+                "posts_per_page" => -1,
+            );
+
+            $loop = new WP_Query($args);
+
+            while ( $loop->have_posts() ):
+                $loop->the_post();
+                ?>
+                <div class="iispmun-committees-card-container">
+                    <div class="iispmun-committee-card expand-on-hover">
+                        <img src="<?php the_post_thumbnail(); ?>">
+                        <div class="iispmun-committee-card-title">
+                            <p><?php the_field("acronym"); ?></p>
+                            <small><?php the_field("difficulty"); ?></small>
+                        </div>
+                    </div>
+                    <div class="iispmun-committee-card-subtitle">
+                        <p>
+                            <span>Topic</span>
+                            <span><?php the_field("short_agenda"); ?></span>
+                        </p>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+</section>
+
+<style>
+    <?php include __DIR__ . "/../committees-card.css"; ?>
+    <?php include "style.css"; ?>
+</style>
