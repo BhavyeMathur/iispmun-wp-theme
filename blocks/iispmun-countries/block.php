ef23fa5edf5daecf8a3737c31b29d4a49b1289b9
Bhavye Mathur <bhavyemathur@gmail.com>
2023-06-19 16:43:16 +0530

    Added a Countries at IISP block
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

---
+++ blocks/iispmun-countries/block.php
@@ -0,0 +1,18 @@
+<section id="countries">
+    <h2>Countries Represented at Indus</h2>
+    <div class="flags-container">
+        <?php
+        $images = acf_photo_gallery("countries", get_the_ID());
+        foreach ($images as $image) { ?>
+            <div class="flag">
+                <img src="<?php echo $image["full_image_url"]; ?>">
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+</section>
+
+<style>
+    <?php include "style.css"; ?>
+</style>
