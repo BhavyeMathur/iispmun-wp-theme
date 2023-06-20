97d3379d148a6493fa947dc769ed79e93c2e463d
Bhavye Mathur <bhavyemathur@gmail.com>
2023-06-20 17:42:48 +0530

    Added an About Us page
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

--- blocks/iispmun-bio/block.php
+++ blocks/iispmun-bio/block.php
@@ -1,29 +1,45 @@
+<?php
+if (block_value("profile-url")) {
+    $bio_id = url_to_postid(block_value("profile-url"));
+} else {
+    $bio_id = get_the_ID();
+}
+?>
+
 <section id="iispmun-bio" class="container-fluid">
     <div class="row">
         <div class="col-md-3 iispmun-card-container">
-            <div class="iispmun-card card-shadow expand-on-hover">
-                <div>
-                    <img class="img-full-size" src="<?php the_field("profile_picture"); ?>">
+            <a href="<?php the_permalink($bio_id); ?>">
+                <div class="iispmun-card card-shadow expand-on-hover">
+                    <div>
+                        <img class="img-full-size" src="<?php the_field("profile_picture", $bio_id); ?>">
+                    </div>
+                    <div class="iispmun-card-title-container">
+                        <p>
+                            <?php echo get_the_title($bio_id); ?>
+                        </p>
+                    </div>
                 </div>
-                <div class="iispmun-card-title-container">
-                    <p>
-                        <?php the_title();?>
-                    </p>
-                </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-md-9 bio-container">
-            <strong>About <?php
-                $title= get_the_title();
-                $arr = explode(' ',trim($title));
-                echo $arr[0];
-                ?>
-            </strong>
+            <?php
+            if (block_value("show-position")): ?>
+                <strong><?php the_field("position", $bio_id); ?>
+                </strong>
+            <?php else: ?>
+                <strong>About <?php
+                    $title = get_the_title($bio_id);
+                    $arr = explode(' ', trim($title));
+                    echo $arr[0];
+                    ?>
+                </strong>
+            <?php endif; ?>
             <p class="paragraph">
                 <?php
                 global $post;
-                $post_content = $post->post_content;
+                $post_content = get_the_content(null, false, $bio_id);
                 $post_content = apply_filters("the_content", $post_content);
                 $post_content = str_replace("</p>", "", $post_content);
                 $paras = explode("<p>", $post_content);
