c904b37b63e3fbe6bb4fd60b16fc9b54648afdc1
Bhavye Mathur <bhavyemathur@gmail.com>
2023-06-20 11:16:45 +0530

    Changed the post-header block to work with single people post type
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

--- blocks/iispmun-post-header/block.php
+++ blocks/iispmun-post-header/block.php
@@ -1,14 +1,26 @@
 <?php setup_postdata(get_the_ID()); ?>
 
 <div class="blog-header">
-    <img src="<?php the_post_thumbnail_url(); ?>">
+    <?php if (get_post_type() == "iispmun_people"): ?>
+        <img style="object-position: bottom" src="<?php the_field("profile_picture"); ?>">
+    <?php else: ?>
+        <img src="<?php the_post_thumbnail_url(); ?>">
+    <?php endif; ?>
+
     <div class="text">
-        <h1>
+        <h1 <?php if (get_post_type() == "iispmun_people") { echo "class='text-uppercase'"; }?>>
             <?php the_title(); ?>
         </h1>
         <h4 class="subtitle">
-            <span class="author"><?php the_author(); ?></span>
-            <?php the_date(); ?>
+            <?php if (get_post_type() == "iispmun_people"): ?>
+                <span class="author"><?php the_field("position"); ?></span>
+            <?php else: ?>
+                <span class="author"><?php the_author(); ?></span>
+            <?php endif; ?>
+
+            <?php if (!get_post_type() == "iispmun_people"):
+                the_date();
+            endif; ?>
         </h4>
     </div>
 </div>
