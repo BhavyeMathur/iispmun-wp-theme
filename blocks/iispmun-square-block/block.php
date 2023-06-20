ce218b7691baa257ea35f90c47b7d0e9f6b1f81d
Bhavye Mathur <bhavyemathur@gmail.com>
2023-06-21 01:14:21 +0530

    Added a square information block
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

---
+++ blocks/iispmun-square-block/block.php
@@ -0,0 +1,10 @@
+<div id="iispmun-square-block" class="width-100"
+     style="background: <?php block_field("background"); ?>;
+             color: <?php block_field("foreground"); ?>">
+    <strong><?php block_field("number"); ?></strong>
+    <p><?php block_field("text"); ?></p>
+</div>
+
+<style>
+    <?php include "style.css"; ?>
+</style>
